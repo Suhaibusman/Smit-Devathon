f92ef13724067c78c522e1791edc827b701f9f2c
Suhaibusman <suhaibusman54@gmail.com>
2023-10-26 13:12:38 -0700

    smit hackathon

---
+++ lib/constants/colors.dart
@@ -0,0 +1,15 @@
+import 'package:flutter/material.dart';
+
+class MyColors {
+  static const Color pinkColor = Color(0xffF77C62);
+  static const Color greenColor = Color(0xff00D6B2);
+
+  static const Color blueColor = Color(0xff1DA9F8);
+  static const Color loghtYellowColor = Color(0xffFFC83A);
+  static const Color greyColor = Color.fromARGB(255, 213, 219, 232);
+  static const Color greyLighColor = Color(0xffF8F9FB);
+  static const Color textGreyColors = Color(0xff8891A5);
+  static const Color headingGreyColors = Color(0xff1E222B);
+  static const Color onBoardText1 = Color(0xffFAFBFD);
+  static const Color onBoardText2 = Color(0xffB2BBCE);
+}
---
+++ lib/constants/images.dart
@@ -0,0 +1,16 @@
+
+
+class Myimages {
+  static const String sadapaycolouredlogo = "assets/images/sadapaycolouredlogo_tp.png";
+ static const String sadapaytextwithlogo = "assets/images/sadapaytextwithlogo.png";
+ static const String sadapaytextwithoutlogo = "assets/images/sadapaytextwithoutlogo.png";
+static const String sadapaywhitelogo = "assets/images/Sadapaywhitelogo.png";
+ static const String recieveicon = "assets/images/recievedicon.png";
+static const String sendicon = "assets/images/sendicon.png";
+static const String jazzlogo = "assets/images/jazzlogo.png";
+static const String waridlogo = "assets/images/waridlogo.png";
+static const String telenorlogo = "assets/images/telenorlogo.png";
+static const String scomlogo = "assets/images/scomlogo.png";
+static const String ufonelogo = "assets/images/ufonelogo.png";
+static const String zonglogo = "assets/images/zong4glogo.png";
+}
